78bf53bcf6b0632d6e2d6abf751d1ea7249b0332
Artem <artemios69soprun@gmail.com>
2022-12-13 14:40:14 +0000

    angular pracrice with ng-content and input

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent {
+
+  
+
+  array:Array<string> = ['comp1','comp2','comp3','comp4','comp5','comp6','comp7','comp8','comp9','comp10']
+
+}
